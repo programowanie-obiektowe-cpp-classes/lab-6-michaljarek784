de3e98c6d3730e96b52966acc47820dfb6325ff5
Michal Jarek <michal.jarek784@wp.pl>
2024-12-07 02:17:41 +0100

    Zmiany

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,19 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    auto it=retval.rbegin();
+    std::transform(people.rbegin(), people.rend(),it, [](Human& person){
+        person.birthday();
+
+        if(person.isMonster())
+        {
+            return 'n';
+        }
+        else
+        {
+            return 'y';
+        }
+    });
 
     return retval;
 }
